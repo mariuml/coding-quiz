06d617c74cf33396f48455a0f4c8f3742319bb22
mariuml <mariumladha97@gmail.com>
2023-01-16 14:18:58 +0000

    Pushing recent code to github for ask BCS

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,4 @@
-
-
 // Iterate over object of questions in a for loop to dispaly on the page
-
-
 
 // GLOBAL VARIABLES: Creating a variable for the start button and screens
 
@@ -12,52 +8,116 @@ var questionsScreen = document.querySelector("#questions");
 var choicesButtons = document.querySelector("#choices");
 var buttons = document.querySelectorAll("button");
 var questionTitle = document.querySelector("#question-title");
+var buttonSelection = 0;
+var questionSelection = 0;
+var wrongAnswer = document.getElementById("#wrong");
+var correctAnswer = document.getElementById("#correct");
+
+// Button variables
+var buttonOne = document.querySelector("#button-1");
+var buttonTwo = document.querySelector("#button-2");
+var buttonThree = document.querySelector("#button-3");
+var buttonFour = document.querySelector("#button-4")
 
 // Adding event listenr to hide start screen and show questions on click
 startButton.addEventListener("click", showFirstQuestionScreen);
 
 
-// FUNCTION: Add answers to buttons - to reuse 
+// FUNCTION: Add answers to buttons - to reuse
 
-function addAnswers (answerNumber) {
-    choicesButtons.classList.remove('hide');
-    choicesButtons.classList.add('show');
-    
-    for (var i=0; i< 4; i++) {
-        buttons[i + 1].textContent = (quizQuestions[answerNumber].choice[i]);
-    }
-        }
-    
-    
-    //  FUNCTION: to show the next question - to reuse
-    
-    function addQuestions(questionNumber) {
-        questionTitle.textContent = (quizQuestions[questionNumber].question);
-    }
-    
+function addAnswers(answerNumber) {
+  choicesButtons.classList.remove("hide");
+  choicesButtons.classList.add("show");
 
-// FUNCTION: to bring up the questions screen 
-
-function showFirstQuestionScreen(){
-
-    // Hide start screen
-    startScreen.remove();
-
-// Make divs visible
-    questionsScreen.classList.remove('hide');
-    questionsScreen.classList.add('show');
-
-// Calling the function - Adding question 1 to the screen
- 
-addQuestions(0);
-
-// Calling the function - Adding question 1 answers to the screen
-addAnswers(0);
-
+  for (var i = 0; i < 4; i++) {
+    buttons[i + 1].textContent = quizQuestions[answerNumber].choice[i];
+    buttons[i + 1].addEventListener("click", function () {
+    });
+  }
 }
 
-// When correct answer is clicked, go to next question - can we create logic to reuse??
+//  FUNCTION: to show the next question - to reuse
 
-// Trigger this function if user selection === correct answer 
+function addQuestions(questionNumber) {
+  questionTitle.textContent = quizQuestions[questionNumber].question;
+}
 
+
+// FUNCTION: to bring up the questions screen
+
+function showFirstQuestionScreen() {
+  // Hide start screen
+  startScreen.remove();
+
+  // Make divs visible
+  questionsScreen.classList.remove("hide");
+  questionsScreen.classList.add("show");
+
+  // Calling the function - Adding question 1 to the screen
+
+  addQuestions(0);
+
+  // Calling the function - Adding question 1 answers to the screen
+  addAnswers(0);
+}
+
+// Event listeners for buttons
+buttonOne.addEventListener("click", function () {
+    buttonSelection = 1;
+    questionSelection ++;
+    return buttonSelection;
+    return questionSelection;
+   
+   });
+   
+   buttonTwo.addEventListener("click", function () {
+     buttonSelection = 2;
+     questionSelection ++;
+     return buttonSelection;
+     return questionSelection;
+   });
+   
+   buttonThree.addEventListener("click", function () {
+       buttonSelection = 3;
+       questionSelection ++; 
+       return buttonSelection;
+       return questionSelection;
+     });
+   
+     buttonFour.addEventListener("click", function () {
+       buttonSelection = 4;
+       questionSelection ++;
+       return buttonSelection;
+       return questionSelection;
+     });
+   
+     console.log(buttonSelection);
+     console.log(questionSelection);
+   
+   // Question 1 logic:
+   
+   if (buttonSelection = 4) {
+       // Feedback 'Correct'
+       console.log("Correct");
+
+//  Display feedback
+
+correctAnswer.classList.remove("hide");
+correctAnswer.classList.add("show");
+
+
+       // Add to score
+       // addAnswers(questionSelection);
+       // addQuestions(questionSelection);
+     } else {
+       // Feedback 'Incorrect'
+// Display feedback
+wrongAnswer.classList.remove("hide");
+wrongAnswer.classList.add("show");
+
+       // Takes away from timer
+       // addAnswers(questionSelection);
+       // addQuestions(questionSelection);
+     }
+     
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -8,45 +8,45 @@ var quizQuestions = [
     "2. Other arrays", 
     "3. Booleans", 
     "4. All of the above"],
-    answer: "All of the above",
+    answer: "4. All of the above",
     },
 
     {
         question: "A very useful debugging tool used during development and debugging front printing content to the debugger is:",
-        choice: ["JavaScript",
-        "Temrinal/bash", 
-        "For loops", 
-        "console.log"],
-        answer: "console.log",
+        choice: ["1. JavaScript",
+        "2. Temrinal/bash", 
+        "3. For loops", 
+        "4. console.log"],
+        answer: "4. console.log",
 
     },
 
     {
         question: "Which method performs a case-sensitive search to determine whether one string may be found within another string?",
-        choice: [".includes()",
-        ".match()", 
-        ".indexOf()", 
-        ".forEach()"],
-        answer: ".includes()",
+        choice: ["1. .forEach() ",
+        "2. .match()", 
+        "3. .indexOf()", 
+        "4. .includes()"],
+        answer: "4. .includes()",
 
     },
     {
         question: "Which operator below is the logical AND?",
-        choice: ["&&",
-        "||", 
-        "!=", 
-        "==="],
-        answer: "&&",
+        choice: ["1. --",
+        "2. ||", 
+        "3. !=", 
+        "4. &&"],
+        answer: "4. &&",
 
     },
 
     {
         question: "Which of one of the terms below is not a data type in JavaScript?",
-        choice: ["undefined",
-        "NaN", 
-        "String", 
-    "Object"],
-        answer: "Object",
+        choice: ["1. undefined",
+        "2. NaN", 
+        "3. String", 
+    "4. Object"],
+        answer: "4. Object",
 
     },
 
